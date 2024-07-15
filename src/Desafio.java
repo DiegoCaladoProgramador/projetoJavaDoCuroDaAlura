639e28be3b0a69f3fa35be9e9c835d0a4ae821d7
DiegoCaladoProgramador <diegocaladoprogramador@gmail.com>
2024-07-15 18:38:25 -0300

    Criado projeto java do curso do alura

---
+++ src/Desafio.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Diego Calado";
+        String tipoConta= "Corrente";
+        double saldo = 2500;
+        String menuInicial = """
+                ****************************************************
+                Dados iniciais do cliente:
+                
+                Nome:           %s
+                Tipo conta:     %s
+                Saldo incial:   %.2f
+                ****************************************************
+                """.formatted(nome, tipoConta, saldo);
+
+        String menu = """
+                Operações
+                
+                1- Consultar saldos
+                2- Receber valor
+                3- Transferir valor
+                4- Sair
+                
+                Digite a opção desejada:
+                """;
+
+        Scanner entrada = new Scanner(System.in);
+        int opcao = 0;
+        System.out.println(menuInicial);
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = entrada.nextInt();
+            if(opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4){
+                System.out.println("Opção inválida");
+            }
+            else if(opcao == 1){
+                System.out.println("o saldo atual é R$ "+ saldo);
+            }
+            else if(opcao == 2){
+                System.out.println("Informe o valor a receber:");
+                double valorRecebido = entrada.nextDouble();
+                saldo += valorRecebido;
+                System.out.println("Saldo atualizado R$ "+ saldo);
+            }
+            else if(opcao == 3){
+                System.out.println("Informe o valor a sacar:");
+                double valorSacar = entrada.nextDouble();
+                if(valorSacar > saldo){
+                    System.out.println("Não há saldo suficiente para fazer esse saque");
+                }
+                else{
+                    saldo -= valorSacar;
+                    System.out.println("Saldo atualizado R$ "+ saldo);
+                }
+            }
+        }
+    }
+}
---
+++ src/Leitura.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite seu filme favorito");
+        String filme = leitor.nextLine();
+        System.out.println("Qual o ano de lançamento?");
+        int anoDeLancamento = leitor.nextInt();
+        System.out.println("Digite sua avaliação para o filme");
+        double avaliacao = leitor.nextDouble();
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
+
+    }
+}
